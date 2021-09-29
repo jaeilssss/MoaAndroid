6360357e63592d5e75ff83ebb6e9d4c35fd773f9
jaeilssss <jaeil95ssss@gmail.com>
2021-09-29 18:34:14 +0900

    contract 뷰 구현시작

---
+++ app/src/main/java/com/moa/moakotlin/ui/partner/PartnerContractReadFragment.kt
@@ -0,0 +1,30 @@
+package com.moa.moakotlin.ui.partner
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.moa.moakotlin.R
+
+class PartnerContractReadFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PartnerContractReadFragment()
+    }
+
+    private lateinit var viewModel: PartnerContractReadViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.partner_contract_read_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(PartnerContractReadViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
