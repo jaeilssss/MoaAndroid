0cb7968d2d72019970ba235b4c68cba34b5c9257
jaeilssss <jaeil95ssss@gmail.com>
2021-10-08 16:09:49 +0900

    imageActivity.kt, 줌 & 아웃 구현중
    실제 테스트 기기 없어서 테스트해보지않음 !
    새로운 액티비티로 이동하는 function 아직없음

--- app/src/main/java/com/moa/moakotlin/viewpageradapter/ConciergeReadViewpagerAdapter.kt
+++ app/src/main/java/com/moa/moakotlin/viewpageradapter/ConciergeReadViewpagerAdapter.kt
@@ -10,8 +10,18 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.moa.moakotlin.R
+import com.moa.moakotlin.base.OnItemClickListener
 
 class ConciergeReadViewpagerAdapter(var context : Context , var list : ArrayList<String>) :  RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    private var mListener : OnItemClickListener?=null
+
+
+    fun setOnItemClickListener(mListener: OnItemClickListener){
+        this.mListener = mListener
+
+    }
+
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -21,9 +31,9 @@ class ConciergeReadViewpagerAdapter(var context : Context , var list : ArrayList
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
-            Glide.with(context).load(list.get(position))
-                    .apply(RequestOptions.bitmapTransform(RoundedCorners(16)))
-                    .into((holder as ConciergeReadViewHolder).image)
+        holder as ConciergeReadViewHolder
+
+        holder.binding(list[position])
 
 
     }
@@ -34,5 +44,28 @@ class ConciergeReadViewpagerAdapter(var context : Context , var list : ArrayList
 
     inner class ConciergeReadViewHolder(var view : View) : RecyclerView.ViewHolder(view){
         var image : ImageView = view.findViewById(R.id.itemConciergeReadImage)
+
+        fun binding(url : String){
+            Glide.with(context).load(url)
+                .apply(RequestOptions.bitmapTransform(RoundedCorners(16)))
+                .into(image)
+            image.setOnClickListener(ButtonClick())
+        }
+
+        inner class ButtonClick : View.OnClickListener{
+            override fun onClick(v: View?) {
+                if(mListener!=null){
+                    when(v?.id){
+                        R.id.itemConciergeReadImage ->{
+                            if(mListener!=null){
+                                if(adapterPosition != RecyclerView.NO_POSITION){
+                                    mListener!!.onItemClick(v,adapterPosition)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
